acaab70d7436d0cc8f03aa961c8438218857ccfc
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-20 14:13:42 +0600

    update Solution

--- Area-Of-Circle.cpp
+++ Area-Of-Circle.cpp
@@ -6,3 +6,14 @@
 //                Topic    : [Area of a Circle]
 //                File     :Area-Of-Circle.cpp
 // ================================================
+#include <iostream>
+#include <iomanip>
+using namespace std ;
+int main(){
+    double R;
+    cin>>R;
+    double area  = 3.141592653 * R*R ;
+    cout<< fixed << setprecision(9);
+    cout<< area ;
+    return 0;
+}
